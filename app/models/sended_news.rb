f9420d31c028d971f3e22d7d1e7b3a83b13f96e4
Muhammed <muhammed@kekik.com.tr>
2022-06-14 18:25:52 +0300

    Added Sended News model and data tables Added activeadmin

---
+++ app/models/sended_news.rb
@@ -0,0 +1,2 @@
+class SendedNews < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_08_202239) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_14_105131) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_08_202239) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "sended_news", force: :cascade do |t|
+    t.string "context"
+    t.string "description"
+    t.string "title"
+    t.string "tag"
+    t.string "images"
   end
 
   create_table "users", force: :cascade do |t|
