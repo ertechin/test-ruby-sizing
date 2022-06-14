87476e4a17a697211fb9f11abaab8503cd2bebdc
Muhammed <muhammed@kekik.com.tr>
2022-06-15 01:20:34 +0300

    Add Donation Model and admin panel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_14_154403) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_14_220650) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -77,6 +77,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_14_154403) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "donations", force: :cascade do |t|
+    t.integer "gyear"
+    t.integer "class_count", default: 0
+    t.integer "donate_count", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sended_news", force: :cascade do |t|
