36688ebbd5708311e2b8309beb5758e4841977d8
Muhammed <muhammed@kekik.com.tr>
2022-06-23 15:02:31 +0300

    Add delete method for added news images

--- app/controllers/added_news_controller.rb
+++ app/controllers/added_news_controller.rb
@@ -11,6 +11,13 @@ class AddedNewsController < ApplicationController
     redirect_to admin_added_news_path(@added_new.id)
   end
 
+  def delete_image
+    image = ActiveStorage::Attachment.find_by(id: params[:id])
+    image.purge
+    added_new = AddedNews.find(image.record_id)
+    redirect_to edit_added_news_path(added_new.id)
+  end
+
   private
   def set_added_news
     @added_new = AddedNews.find(params[:id])
