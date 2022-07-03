d760d7e6e48a40e86db6ce80e52d2716cd5053ca
Ersel Ertekin <ersel@kekik.com.tr>
2022-07-03 22:47:55 +0300

    refactor donations by mvc pattern

--- app/controllers/api/v1/donations_controller.rb
+++ app/controllers/api/v1/donations_controller.rb
@@ -1,15 +1,9 @@
-# Controller for Donations fields
 class Api::V1::DonationsController < ApiController
+
   def take_donations
-    gyear = JSON.parse(params[:gyear])
-    last_donate_year = Donation.limit(1).order(updated_at: :desc)
-    biggest_five_donations = Donation.limit(5).order(donate_count: :desc)
-    user_gyear_donations = Donation.where(gyear:)
-    render json: {
-      biggest_five_donations:,
-      last_donate_year:,
-      user_gyear_donations:,
-      request_id: request.request_id
-    }
+    @last_donate_year = Donation.last_donate_year
+    @biggest_five_donations = Donation.biggest_five_donations
+    @user_gyear_donations = Donation.user_gyear_donations(params)
   end
+  
 end
--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,2 +1,15 @@
 class Donation < ApplicationRecord
+
+  def self.last_donate_year
+    @last_donate_year = Donation.limit(1).order(updated_at: :desc)
+  end
+
+  def self.biggest_five_donations
+    @biggest_five_donations = Donation.limit(5).order(donate_count: :desc)
+  end
+
+  def self.user_gyear_donations(params)
+    @user_gyear_donations = Donation.where(gyear: params[:gyear])
+  end
+
 end
