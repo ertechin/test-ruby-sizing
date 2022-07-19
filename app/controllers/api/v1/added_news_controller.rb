eee80e73e94f29344be94b708a4e07d6dbc10241
Ersel Ertekin <ersel@kekik.com.tr>
2022-07-19 11:53:13 +0300

    do pr comment

--- app/controllers/api/v1/added_news_controller.rb
+++ app/controllers/api/v1/added_news_controller.rb
@@ -4,34 +4,19 @@ class Api::V1::AddedNewsController < ApiController
     case params[:tag]
     when 'all'
       res = AddedNews.all
-      @data = result_modifier(res)
+      @data = AddedNews.result_modifier(res)
     else
       res = AddedNews.where(tag: params[:tag])
-      @data = result_modifier(res)
+      @data = AddedNews.result_modifier(res)
     end
   end
 
   def search_news
     res = AddedNews.search(params)
-    @search_result = result_modifier(res)
+    @search_result = AddedNews.result_modifier(res)
   end
 
   def search_news_params
     params.require(:api_news).permit(:query)
-  end
-
-  def result_modifier(res)
-    res.each do |e|
-      images_urls = ActiveStorage::Attachment.where(record_type: 'AddedNews', record_id: e.id)
-      if images_urls.any?
-        return_urls = images_urls.map do |image|
-          image.url
-        end
-        e.image_urls = return_urls
-        res
-      else
-        res
-      end
-    end
   end
 end
